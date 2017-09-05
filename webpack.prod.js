51077a0b5b5d134106bb35db12c28fd2d561d852
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2017-09-05 08:27:13 -0300

    Use firebase

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,16 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = merge(common, {
   plugins: [
-    new UglifyJSPlugin()
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
   ]
 });
 
