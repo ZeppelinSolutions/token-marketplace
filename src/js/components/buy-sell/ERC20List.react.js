731fa27704d6f4b4699329403501cc4e468ff49e
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2017-09-12 01:23:08 -0300

    Improve MyToken implementation

--- src/js/components/buy-sell/ERC20List.react.js
+++ src/js/components/buy-sell/ERC20List.react.js
@@ -14,7 +14,7 @@ export default class DeployContractForm extends React.Component {
   render() {
     return (
       <div className="row erc20-list">
-        <a className="btn btn-floating option tooltipped" id="0xad13f9da729a960a2724e7a34c9ab03eedd6959e" data-position="bottom" data-delay="50" data-tooltip="MyToken" onClick={this.selectERC20}>MTK</a>
+        <a className="btn btn-floating option tooltipped" id="0x7d7079ae4985c58420e41dec33768a56c6b2cb25" data-position="bottom" data-delay="50" data-tooltip="MyToken" onClick={this.selectERC20}>MTK</a>
         <a className="btn btn-floating option tooltipped" id="0xa0969323ce4d1826ddeaa451c4c1f0c9c67d3762" data-position="bottom" data-delay="50" data-tooltip="Banana Coin" onClick={this.selectERC20}>BAC</a>
         <a className="btn btn-floating option tooltipped" id="0xb5de0b4cbf397209e4badafa7f31aec57518cebc" data-position="bottom" data-delay="50" data-tooltip="Whatever" onClick={this.selectERC20}>WTV</a>
         <a className="btn btn-floating option tooltipped" id="0x1cd78C8BD5E021ABB1D8561a077263Bc8eb54481" data-position="bottom" data-delay="50" data-tooltip="SampleToken" onClick={this.selectERC20}>SAT</a>
